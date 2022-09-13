bc58278f03c6604b87e6908e31e7591aa22d5520
Tlapster <113150603+Tlapster@users.noreply.github.com>
2022-09-12 19:34:34 -0500

    My first page

---
+++ D3_D6/day-6.js
@@ -0,0 +1,4 @@
+/* Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos los años que ha cumplido (desde 1 hasta su edad).*/
+
+/* Mejora tu programa tanto como el tiempo te lo permita.
+Comparte un screenshot de tu programa funcionando por discord, no compartas tu codigo, solo los prints de tu resultado*/
