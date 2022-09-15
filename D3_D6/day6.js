5f77b38d40ba71ab6350ac4695223f22d7af0599
Tlapster <113150603+Tlapster@users.noreply.github.com>
2022-09-14 20:28:55 -0500

    marca 14 de septiembre

---
+++ D3_D6/day6.js
@@ -0,0 +1,16 @@
+/* Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos los años que ha cumplido (desde 1 hasta su edad).*/
+
+/* Mejora tu programa tanto como el tiempo te lo permita.
+Comparte un screenshot de tu programa funcionando por discord, no compartas tu codigo, solo los prints de tu resultado*/
+
+
+let edad = parseInt(prompt("ingresa tu edad. "));
+
+let i = 1;
+
+while (edad >= i) {
+    console.log(i)
+    //antes de terminar, sumar 1 a i
+
+    i= i+1
+}
