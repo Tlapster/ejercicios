98ed46d90428de27b6f6143567b398ff4bdf76ad
Tlapster <113150603+Tlapster@users.noreply.github.com>
2022-09-16 15:31:15 -0500

    New we (#2)
    * git
    
    * My first page
    
    * My firstweb page
    
    * My first webpage
    
    * marca 14 de septiembre
    
    * 16 de sept
    
    * 16 de septiembre
    
    * nuevo index

---
+++ D2_D7/D7/day-7.js
@@ -0,0 +1,15 @@
+// Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, 
+// Física, Química, Historia y Lengua) en un array y la muestre por pantalla.
+
+/* Mejora tu programa tanto como el tiempo te lo permita (que lo muestre uno por uno, que lo muestre contatenado con una indicacion, etc).
+Comparte un screenshot de tu programa funcionando por discord, no compartas tu codigo, solo los prints de tu resultado*/
+
+let item1 = (prompt("Escribe tu primer materia, "));
+let item2 = (prompt("Escribe tu segunda materia, "));
+let item3 = (prompt("Escribe tu tercer materia, "));
+let item4 = (prompt("Escribe tu cuarta materia, "));
+let item5 = (prompt("Escribe tu quinta materia, "));
+
+const array_materias =("1.-" + item1 + " " + "2.-" + " " + item2 + " " + "3.-"+ " " + item3 + " " + "4.-" + " " + item4 + " " + "5.-" + " " + item5);
+
+  console.log(alert(array_materias))
