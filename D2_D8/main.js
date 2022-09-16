8114eccf487bc79bb58775eed127dc8dbb12e5c5
Tlapster <113150603+Tlapster@users.noreply.github.com>
2022-09-16 13:42:29 -0500

    16 de sept

---
+++ D2_D8/main.js
@@ -0,0 +1,29 @@
+// Escribir un programa que almacene en una lista los números del 1 al 10 
+// y los muestre por pantalla en orden inverso separados por comas.
+
+/* Mejora tu programa tanto como el tiempo te lo permita .
+Comparte un screenshot de tu programa funcionando por discord, no compartas tu codigo, solo los prints de tu resultado*/
+/*
+let num1 = (prompt("Escribe el número: 1"));
+let num2 = (prompt("Escribe el siguiente número"));
+let num3 = (prompt("Escribe el siguiente número"));
+let num4 = (prompt("Escribe el siguiente número"));
+let num5 = (prompt("Escribe el siguiente número"));
+let num6 = (prompt("Escribe el siguiente número"));
+let num7 = (prompt("Escribe el siguiente número"));
+let num8 = (prompt("Escribe el siguiente número"));
+let num9 = (prompt("Escribe el siguiente número"));
+let num10 = (prompt("Por último escribe el número 10"));
+
+const numeros = [num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10];
+
+
+
+console.log(alert("El conteo en decremento es: "+ (numeros.reverse())))
+*/
+
+let numeracion = [1,2,3,4,5,6,7,8,9,10];
+
+numeracion =numeracion.reverse();
+
+console.log(alert(numeracion))
