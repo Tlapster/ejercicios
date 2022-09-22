c318fc3b39e0fd9aa6dced9a03c36a2edde7e0fc
Your Name <you@example.com>
2022-09-22 12:05:40 -0500

    22 de septiembre

---
+++ D2_D12/day-12.js
@@ -0,0 +1,68 @@
+/* 
+
+CONTANDO OVEJAS
+
+Pista: Para comenzar con el reto, concentrate primero en un filtro, luego vas añadiendo el resto, pero comienza con uno solamente.
+
+Recuerdas el ejercicio que vimos en la mañana? Vamos a hacer algo similar pero tambien añadiremos un par de cosas.
+
+En este programa que crearás, se te dará un array de donde hay ovejas, cad oveja tiene dos caracteristicas: nombre y color, en ese orden respectivamente.
+
+Tu trabajo es mostrar en pantalla o consola, solo las ovejas que sean de color 'rojo', y que su nombre incluya las letras 'n' y 'a', sin importar el orden o espacios.
+
+
+Extra:
+
+Incluye las ovejas que incluan 'N' y 'A' mayusculas.
+
+En caso te sientas atascado en alguna parte, no dudes en pedir ayuda en el chat de DISCORD :D
+
+*/
+
+
+const ovejas = [
+  // [nombre, color]
+  ['Noa','rojo'],
+  ['Euge','rojo'],
+  ['Navidad','azul'],
+  ['Ki Na Ma','azul'],
+  ['AAAAAaaaaa','rojo'],
+  ['Nnnnnnnn','azul'],
+  ['AannnNN','azul'],
+  ['NEmaosA','rojo'],
+  ['NerufgE','rojo'],
+  ['Anrtoa','rojo'],
+  ['Aiedii','azul'],
+  ['Nn aEst','azul'],
+  ['Perstesto','rojo'],
+  ['Nidivad','azul'],
+  ['Mistery','rojo'],
+  ['An na nas','azul'],
+  ['Varias','rojo'],
+  ['Misiones','azul'],
+  ['Aladas','rojo'],
+  ['njavascript','rojo'],
+  ['Python','azul'],
+  ['ReactJS','rojo'],
+  ['Julian','rojo'],
+  ['Mitificando','azul'],
+  ['astilleria','rojo'],
+  ['Ejercicios','azul'],
+  ['antioquia','rojo'],
+  ['vertigo','rojo'],
+  ['Molinillo','rojo'],
+  ['mauirosl','azul'],
+  ['melamina','rojo'],
+  ['Atascando','azul'],
+]
+
+const nombre= 0;
+const color= 1;
+
+
+for(let oveja of ovejas){
+  if (oveja = oveja[color]);
+  else(oveja = oveja )
+  console.log(oveja);
+  
+}
---
+++ D2_D13/day-13.js
@@ -0,0 +1,35 @@
+/* 
+Hoy tienes el ultimo ejercicio de JavaScript de este nivel.
+
+Veamos una nueva forma de usar las cosas que aprendiste.
+
+Hoy la tarea será sobre unir dos arrays en uno.
+
+INSTRUCCIONES
+
+Dados 2 arrays, imprime en consola un tercer array que tenga los datos del primer array y del segundo. Puedes usar ciclos, metodos segun sea conveniente.
+
+También puedes buscar en la documentación de W3Schools, si tienes dudas puedes preguntar al grupo de discord :D
+
+Ejemplo:
+
+array1 : ["Pedro", "Marcos", "Maria", "Ignacio"]
+array2 : ["Carla", "Gonzalo", "Piero", "Mercedes"]
+
+array3 : ["Pedro", "Marcos", "Maria", "Ignacio", "Carla", "Gonzalo", "Piero", "Mercedes"]
+
+
+Mejora tu programa tanto como el tiempo te lo permita.
+Comparte un screenshot de tu programa funcionando por discord, no compartas tu codigo, solo los prints de tu resultado
+
+*/
+
+const a1 = ["Pedro", "Marcos", "Maria", "Ignacio"]
+console.log(a1);
+
+const a2 = ["Carla", "Gonzalo", "Piero", "Mercedes"]
+console.log(a2)
+
+const suma1_2 = a1.concat(a2);
+
+console.log(suma1_2);
