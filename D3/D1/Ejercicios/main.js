b95a285d789b1af12dec07714b804aab85adc045
Tlapster <113150603+Tlapster@users.noreply.github.com>
2022-10-03 19:47:54 -0500

    D3

---
+++ D3/D1/Ejercicios/main.js
@@ -0,0 +1,8 @@
+let respuesta = promt("Cual es el nombre de Superman?");
+const resultado = document.getElementById("resultado");
+
+if(respuesta.tolowercase()==="clark kent"){
+    resultado.textContent = "correcta!";
+}   else{
+    
+}
